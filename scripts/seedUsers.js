f3a480838ae2b0212f477db97ded421b1e0769c1
Ross McLane <ross.mclane@gmail.com>
2019-12-20 11:42:38 -0500

    Seeding multiple users

--- scripts/seedUsers.js
+++ scripts/seedUsers.js
@@ -3,13 +3,23 @@ const db = require("../models");
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mapper");
 
+var users = [
+  {
+    username: "rossmclane",
+    maps: []
+  },
+  {
+    username: "daughtrystjohn",
+    maps: []
+  },
+  {
+    username: "jimmyfallon",
+    maps: []
+  }
+];
+
 db.User.remove({})
-  .then(() =>
-    db.User.create({
-      username: "rossmclane",
-      maps: []
-    })
-  )
+  .then(() => db.User.create(users))
   .then(data => {
     console.log(data);
     process.exit(0);
